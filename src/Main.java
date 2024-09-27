b405b17f1b9a1a9a69cd5e0b70770fcbdc597263
BT_2S14_10 <@email>
2024-09-27 10:43:44 -0400

    -Added Scanner setup and asks user for bill -Added asking the user for the amount of tip in percent -Added asking the user for the amount of people paying -Added Computer computing calculations for all the final values -Added Final bill outputs and scanner close -Added a few more descriptive notes -Fixed rounding decimal places to cent and organized code a little

--- src/Main.java
+++ src/Main.java
@@ -2,39 +2,49 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         //Scanner setup and bill information entering
         Scanner s = new Scanner(System.in);
         System.out.println("Tip Calculator");
         System.out.println("----------------");
         System.out.println("Enter the bill($): ");
-        String bill = s.nextLine();
-        double billConverted = Double.parseDouble(bill);
+        String billString = s.nextLine();
+        double bill = Double.parseDouble(billString);
+        String billRounded = String.format("%.2f",bill);
+        double billConverted = Double.parseDouble(billRounded);
         System.out.println("----------------");
+
         //Tip amount entering
         System.out.println("Enter how much tip (without percent): ");
-        String tip = s.nextLine();
-        double tipConverted = Double.parseDouble(tip);
+        String tipString = s.nextLine();
+        double tip = Double.parseDouble(tipString);
+        String tipRounded = String.format("%.2f",tip);
+        double tipConverted = Double.parseDouble(tipRounded);
         System.out.println("----------------");
+
         //Amount of people paying
         System.out.println("Enter how many people are paying: ");
         String amountOfPeople = s.nextLine();
         int amountOfPeopleConverted = Integer.parseInt(amountOfPeople);
         System.out.println("----------------");
+
         //Computer Calculations
         double tipInPercent = (tipConverted * 0.01);
         double finalTip = (tipInPercent * billConverted);
         double finalBillTotal = (finalTip + billConverted);
         double totalPaidPerPerson = (finalBillTotal / amountOfPeopleConverted);
         double tipPerPerson = (finalTip / amountOfPeopleConverted);
+
         //Final calculator output
-        System.out.println("Your total bill is: $" + finalBillTotal);
+        System.out.println("Your total bill is: $" + String.format("%.2f",finalBillTotal));
         System.out.println("---------------------------");
-        System.out.println("The tip amount is: $" + finalTip);
+        System.out.println("The tip amount is: $" + String.format("%.2f",finalTip));
         System.out.println("---------------------------");
+
         //This part is to add to the output when there are more than 1 person paying.
         if (amountOfPeopleConverted > 1) {
-            System.out.println("Each person will pay: $" + totalPaidPerPerson);
-            System.out.println("The tip per person is: $" + tipPerPerson);
+            System.out.println("Each person will pay: $" + String.format("%.2f",totalPaidPerPerson));
+            System.out.println("The tip per person is: $" + String.format("%.2f",tipPerPerson));
             System.out.println("---------------------------");
         }
         s.close();
